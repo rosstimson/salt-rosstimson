a73dbaa65d5cd7334c4dd39c4d10cf40304f0ba0
Ross Timson <ross@rosstimson.com>
2016-03-29 20:57:34 +0100

    First stab at OpenBSD & FreeBSD compatible bootstrap script.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,18 +2,36 @@
 
 set -eux
 
-SALT_ROOT=/usr/local/etc/salt
+case $(uname) in
+  FreeBSD)
+    SALT_ROOT=/usr/local/etc/salt
+  ;;
+  OpenBSD)
+    SALT_ROOT=/etc/salt
+  ;;
+esac
 
 # Prerequisites
-env ASSUME_ALWAYS_YES=YES pkg bootstrap
-pkg install -y ca_root_nss
+case $(uname) in
+  FreeBSD)
+    env ASSUME_ALWAYS_YES=YES pkg bootstrap
+    pkg install -y ca_root_nss
+  ;;
+esac
 
 # Install Salt
-fetch -o /tmp/install_salt.sh https://bootstrap.saltstack.com
-chmod +x /tmp/install_salt.sh
-/tmp/install_salt.sh -X -P git v2015.8.7
+case $(uname) in
+  FreeBSD)
+    fetch -o /tmp/install_salt.sh https://bootstrap.saltstack.com
+    chmod +x /tmp/install_salt.sh
+    /tmp/install_salt.sh -X -P git v2015.8.8
+  ;;
+  OpenBSD)
+    pkg_add salt
+  ;;
+esac
 
-# When install salt with git option a minion config is created rather than a
+# When installing salt with git option a minion config is created rather than a
 # minion.sample.  Check for it and backup before writing own.
 [ -f ${SALT_ROOT}/minion ] && \
   cp ${SALT_ROOT}/minion ${SALT_ROOT}/minion.orig
@@ -38,8 +56,16 @@ mkdir -p ${SALT_ROOT}/pillar
 # Fetch tarball of Salt state repo and copy into correct locations.  The
 # project is organised a little odd due to kitchen-salt, just copy what is
 # needed rather than everything.
-fetch -o /tmp/salt-rosstimson.tar.gz \
-  https://github.com/rosstimson/salt-rosstimson/archive/master.tar.gz
+case $(uname) in
+  FreeBSD)
+    fetch -o /tmp/salt-rosstimson.tar.gz \
+      https://github.com/rosstimson/salt-rosstimson/archive/master.tar.gz
+  ;;
+  OpenBSD)
+    ftp -o /tmp/salt-rosstimson.tar.gz \
+      https://github.com/rosstimson/salt-rosstimson/archive/master.tar.gz
+  ;;
+esac
 tar xzvf /tmp/salt-rosstimson.tar.gz -C /tmp
 
 cd /tmp/salt-rosstimson-master
